e308c92c0b43573e5b25cff91e678fa1e7d03ff4
Mrexyha <legeta.vasilina06@gmail.com>
2025-02-05 14:47:31 +0200

    #FS-13 - add Progress Tracker and Google Speech Integration

---
+++ src/DAL/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        User GetById(int id);
+        List<User> GetAll();
+        void Add(User user);
+        void Update(User user);
+        void Delete(int id);
+    }
+}
---
+++ src/ForSpeak/Integrations/GoogleSpeechIntegration.cs
@@ -0,0 +1,11 @@
+﻿namespace ForSpeak.Integrations
+{
+    public class GoogleSpeechIntegration
+    {
+        public string RecognizeSpeech(byte[] audioData)
+        {
+            // Integrate with Google Speech API to recognize speech
+            return "Recognized Text";
+        }
+    }
+}
