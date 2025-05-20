3442435069678daf429997242446d0ae1a19f391
Mrexyha <legeta.vasilina06@gmail.com>
2025-05-21 00:05:08 +0300

    correct displaying total points in chart

--- src/View/for-speak/src/app/services/api.ts
+++ src/View/for-speak/src/app/services/api.ts
@@ -1,15 +1,28 @@
 import axios from 'axios'
+import router from '../router'
 
 const api = axios.create({
   baseURL: 'https://localhost:7058/api',
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('jwt')
+  const token = localStorage.getItem('token')
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
+
+api.interceptors.response.use(
+  (resp) => resp,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
+      router.push({ name: 'login', query: { redirect: router.currentRoute.value.fullPath } })
+    }
+    return Promise.reject(err)
+  },
+)
 
 export default api
